13eb0645a5c356d586f5c718801ab3559ccbeccc
Marcos <marcos.vinicius.da.silva.pro@gmail.com>
2023-08-15 13:36:47 -0300

    Validação dos métodos de pagamento completa

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,6 +1,19 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
+        let variacaoDePreco = 0;
+        switch(metodoDePagamento) {
+            case "dinheiro":
+                variacaoDePreco = -0.05;
+                break;
+            case "debito":
+                break;
+            case "credito":
+                variacaoDePreco = 0.03;
+                break;
+            default:
+                return "Forma de pagamento inválida!"
+        }
         return "";
     }
 
